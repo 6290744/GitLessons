6041356469c31a1b56df7d8917e5a02611741107
ERisunkov <6290744@gmail.com>
2026-02-11 10:39:37 +0100

    Change cube rotation to translate

--- Assets/Scripts/CubeRotator.cs
+++ Assets/Scripts/CubeRotator.cs
@@ -6,6 +6,6 @@ public class CubeRotator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        this.transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * 10f);
+        this.transform.Translate(Vector3.up * Time.deltaTime);
     }
 }
